6a8442f8ad58d6757c08ebae4bca5e6659a4f0f5
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-05-05 14:09:22 +0200

    add plugin manager

--- nvim/lua/basics.lua
+++ nvim/lua/basics.lua
@@ -1,1 +1,8 @@
+-- leaser key
 vim.g.mapleader = ','
+
+-- tab behaviour
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,2 +1,33 @@
+-- Lazy will be my plugin manager
+-- see https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+ , { "nvim-tree/nvim-tree.lua",
+     version = "*",
+     lazy = false,
+     dependencies = {
+       "nvim-tree/nvim-web-devicons",
+     }
+  }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
 require('nvim-tree').setup()
-require'nvim-web-devicons'.setup()
