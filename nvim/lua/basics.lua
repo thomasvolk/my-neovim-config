08f0a8a84613ee4329d3e65986b176db0f11cb37
Thomas Volk <thomas.volk@trustedshops.de>
2024-05-07 18:23:46 +0200

    add short cuts and line numbers

--- nvim/lua/basics.lua
+++ nvim/lua/basics.lua
@@ -6,3 +6,10 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+
+-- show linenumbers
+vim.wo.number = true
+
+-- shortcuts
+vim.keymap.set('n', '<C-b>', ':bn<CR>', {})
+vim.keymap.set('n', '<C-.>', ':q<CR>', {})
