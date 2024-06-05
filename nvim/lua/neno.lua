41fa0ebfd70596115b1ecb14337ace92d0d41445
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2024-06-05 19:09:34 +0200

    improve pattern matching for wiki link

--- nvim/lua/neno.lua
+++ nvim/lua/neno.lua
@@ -7,7 +7,7 @@ require("subtext")
 vim.api.nvim_create_autocmd({"TextChanged", "BufEnter", "BufWinEnter"}, {
     pattern = "*.subtext",
     callback = function()
-        vim.cmd.syntax([[match NenoRef "\v\[\[[0-9A-Za-z_\- ]+\]\]+"]])
+        vim.cmd.syntax([[match NenoRef "\v\[\[.{-}\]\]+"]])
         vim.cmd.highlight([[NenoRef guifg=#88EE88 gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen]])
         vim.cmd.syntax([[match NenoFile "\v(^|\s)/[a-zA-Z0-9\-_/\.]+($|\s)"]])
         vim.cmd.highlight([[NenoFile guifg=#88EEEE gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen]])
