6a6d5c76bcefc1ca950314eb43975986d10f05b6
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2024-06-05 21:53:44 +0200

    get wiki link below cursor

--- nvim/lua/subtext.lua
+++ nvim/lua/subtext.lua
@@ -7,6 +7,24 @@ local jump_to_note = function ()
   if string.find(noteName, "^/[%w-]+") then
     local path = "." .. noteName .. ".subtext"
     vim.cmd("e " .. path)
+  else
+    local column = vim.api.nvim_win_get_cursor(0)[2]
+    local line = vim.api.nvim_get_current_line()
+    local right = string.sub(line, column + 1, string.len(line))
+    local left = string.sub(line, 0, column)
+    local r_wiki_bracket = string.find(right, "%]%]")
+    if r_wiki_bracket then
+      local l_wiki_bracket = string.find( (left):reverse(), "%[%[")
+      if l_wiki_bracket then
+        local e = column + r_wiki_bracket - 1
+        local s = column - l_wiki_bracket + 2
+        local link = string.sub(line, s, e)
+        vim.notify(link)
+      end
+    end
+
+
+    -- vim.notify(left .. "|-----|" .. right)
   end
 end
 
