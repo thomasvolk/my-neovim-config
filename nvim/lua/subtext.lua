286578a7b61a3d1d116005b8a6dfc1a8ad1a0fba
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-06-05 20:10:41 +0200

    fix url regex

--- nvim/lua/subtext.lua
+++ nvim/lua/subtext.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd({"TextChanged", "BufEnter", "BufWinEnter"}, {
     callback = function()
         vim.cmd.syntax([[match SubtextHeading "\v^#.+$"]])
         vim.cmd.highlight([[SubtextHeading guifg=#8888FF gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen]])
-        vim.cmd.syntax([[match SubtextUrl "\vhttps?://[/\-a-zA-Z0-9@:%._\+~#=\.]+($|\s)"]])
+        vim.cmd.syntax([[match SubtextUrl "\v(^|\s)https?://[/\-a-zA-Z0-9@:%._\+~#=\.]+($|\s)"]])
         vim.cmd.highlight([[SubtextUrl guifg=#EE8888 gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen]])
         vim.cmd.syntax([[match SubtextRef "\v(^|\s)/[a-zA-Z0-9\-_/]+($|\s)"]])
         vim.cmd.highlight([[SubtextRef guifg=#88EE88 gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen]])
