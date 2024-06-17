c46b17eb965b01f1650f0c241219ee099a774718
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-06-17 17:02:18 +0200

    improve cmd source

--- nvim/lua/subtext_cmp_source.lua
+++ nvim/lua/subtext_cmp_source.lua
@@ -33,10 +33,13 @@ source.new = function()
 end
 
 function source:complete(request, callback)
-  local notes = list_subtext_files()
   local items = {}
-  for _, note in ipairs(notes) do
-    table.insert(items, { label = note, kind = require('cmp.types').lsp.CompletionItemKind.Reference })
+  local line = request.context.cursor_before_line
+  if string.sub(line, -1) == "/" or string.sub(line, -2) == "[[" or line == ":alias-of:" then
+    local notes = list_subtext_files()
+    for _, note in ipairs(notes) do
+      table.insert(items, { label = note, kind = require('cmp.types').lsp.CompletionItemKind.Reference })
+    end
   end
   callback({ items = items })
 end
