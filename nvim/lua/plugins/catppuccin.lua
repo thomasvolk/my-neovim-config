afb746a7af5537568febd3b11da87542a94262ac
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-04-05 16:03:16 +0200

    change default to light

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,10 +5,10 @@ return {
   priority = 1000,
   config = function()
     local theme = os.getenv("CONSOLE_THEME")
-    if theme and string.lower(theme) == "dark" then
-      vim.cmd.colorscheme "catppuccin-frappe"
-    else
+    if theme and string.lower(theme) == "light" then
       vim.cmd.colorscheme "catppuccin-latte"
+    else
+      vim.cmd.colorscheme "catppuccin-frappe"
     end
   end
 }
