d8b5ad6c4127f305ad336b4e23df68a2bd24a1f7
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-04-05 09:25:25 +0200

    back to frappe style

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,10 +4,6 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    if os.getenv("NVIM_THEME") == "dark" then
-      vim.cmd.colorscheme "catppuccin-frappe"
-    else
-      vim.cmd.colorscheme "catppuccin-latte"
-    end
+    vim.cmd.colorscheme "catppuccin-frappe"
   end
 }
