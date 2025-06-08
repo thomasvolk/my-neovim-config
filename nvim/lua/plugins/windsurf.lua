ef82b6a215c5e017278effb0258b852fcec55e15
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-06-08 10:14:14 +0200

    disable dodeium by default

--- nvim/lua/plugins/windsurf.lua
+++ nvim/lua/plugins/windsurf.lua
@@ -5,5 +5,7 @@ return {
     vim.keymap.set('i', '<F2>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
     vim.keymap.set('n', '<F3>', ":Codeium Enable<CR>", {})
     vim.keymap.set('n', '<F4>', ":Codeium Disable<CR>", {})
+    -- disable codeium by default
+    vim.g.codeium_enabled = false
   end
 }
