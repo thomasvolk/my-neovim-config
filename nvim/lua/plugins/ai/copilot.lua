9d0fd90d38675918af4a0ae8bdd0f1b40ab850a7
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2025-12-29 13:37:39 +0100

    remove v8 branch

--- nvim/lua/plugins/ai/copilot.lua
+++ nvim/lua/plugins/ai/copilot.lua
@@ -63,7 +63,6 @@ return {
     {
       -- The GitHub repository for the codecompanion.nvim plugin.
       "olimorris/codecompanion.nvim",
-      branch = 'v18',
 
       -- Specifies other plugins that codecompanion.nvim needs to function correctly.
       dependencies = {
