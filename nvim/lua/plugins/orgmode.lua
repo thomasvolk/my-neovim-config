63601003660ee25e39186bcc91a39de441a94c35
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-05-01 08:53:15 +0200

    improve org mode

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -5,6 +5,8 @@ return {
     require('orgmode').setup({
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
+      org_startup_folded = 'content',
     })
+    vim.lsp.enable('org') -- experimental
   end,
 }
